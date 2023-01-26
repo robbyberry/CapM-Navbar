851cdc859cd87601297afc92f87ea3f2ad93ce78
robbyberry <87150464+robbyberry@users.noreply.github.com>
2023-01-26 13:01:37 -0500

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -11,6 +11,73 @@ $(document).ready(function(){
           </a>
       </li>
         <li>
+          <a href="#">Pipeline & Inventory</a>
+          <div class="uk-navbar-dropdown">
+            <ul class="uk-nav uk-navbar-dropdown-nav">
+                 <li class="uk-nav-header">Portfolio</li>
+              <li><a href="https://www.golinks.io/expecteddebt" rel="noopener noreferrer" target="_parent" >Expected Debt</a></li>
+              <li><a href="https://www.golinks.io/currinv" rel="noopener noreferrer" target="_parent" >Current Inventory</a></li>
+              <li><a href="https://www.golinks.io/utidata" rel="noopener noreferrer" target="_parent" >UTI Inventory</a></li>
+                <li class="uk-nav-divider"></li>
+                   <li class="uk-nav-header">Pipeline</li>
+              <li><a href="https://www.golinks.io/osnpipeline" rel="noopener noreferrer" target="_parent" >OSN Acquisition Pipeline</a></li>
+              <li><a href="https://www.golinks.io/osnresalepipeline" rel="noopener noreferrer" target="_parent" >OSN Resale Pipeline</a></li>
+            </ul>
+          </div>
+        </li>
+        <li>
+          <a href="#">Ops Dashboards</a>
+          <div class="uk-navbar-dropdown">
+            <ul class="uk-nav uk-navbar-dropdown-nav">
+                <li class="uk-nav-header">Reporting</li>
+              <li><a href="https://www.golinks.io/termresold" rel="noopener noreferrer" target="_parent" >Term Resold Reports</a></li>
+              <li><a href="https://www.golinks.io/assetschedules" rel="noopener noreferrer" target="_parent" >Facility Asset Schedules</a></li>
+              <li><a href="https://www.golinks.io/notallocated" rel="noopener noreferrer" target="_parent" >Reporting Tokens Not Allocated</a></li>
+              <li><a href="https://www.golinks.io/mezzpendingreleases" rel="noopener noreferrer" target="_parent" >Mezz Pending Releases</a></li>
+              <li class="uk-nav-divider"></li>
+                   <li class="uk-nav-header">Ops Favorites</li>
+              <li><a href="https://www.golinks.io/opsmasterfile" rel="noopener noreferrer" target="_parent" >Ops Master File</a></li>
+              <li class="uk-nav-divider"></li>
+                   <li class="uk-nav-header">Valuations</li>
+              <li><a href="https://www.golinks.io/bpometrics" rel="noopener noreferrer" target="_parent" >BPO Metrics Report</a></li>
+               <li><a href="https://www.golinks.io/completebpos" rel="noopener noreferrer" target="_parent" >All Completed Valuations</a></li>
+            </ul>
+          </div>
+        </li>
+         <li>
+          <a href="#">Deal Dashboards</a>
+          <div class="uk-navbar-dropdown">
+                    <ul class="uk-nav uk-navbar-dropdown-nav">
+                        <li class="uk-nav-header">Deal Favorites</li>
+                            <li><a href="https://www.golinks.io/dealmasterfile" rel="noopener noreferrer" target="_parent" >Deal Team Master File</a></li>
+                            <li><a href="https://www.golinks.io/kkrbiweekly" rel="noopener noreferrer" target="_parent" >KKR Biweekly Report</a></li>
+                            <li><a href="https://www.golinks.io/mezzqueries" rel="noopener noreferrer" target="_parent" >Mezz Queries</a></li>
+                        <li class="uk-nav-divider"></li>
+                        <li class="uk-nav-header">Other - Placeholder</li>
+                    </ul>
+                </div>
+            </li>
+             <li>
+          <a href="#">Portfolio Health</a>
+          <div class="uk-navbar-dropdown">
+            <ul class="uk-nav uk-navbar-dropdown-nav">
+              <li><a href="https://www.golinks.io/bbanalytics" rel="noopener noreferrer" target="_parent" >Borrowing Base Analytics</a></li>
+              <li><a href="https://www.golinks.io/3momargin" rel="noopener noreferrer" target="_parent" >3mo Trailing Gross Margin</a></li>
+              <li><a href="https://www.golinks.io/agedcollat" rel="noopener noreferrer" target="_parent" >Aged Collateral Test</a></li>
+            </ul>
+          </div>
+        </li>
+        <li>
+          <a href="#">Transactions & Cash</a>
+          <div class="uk-navbar-dropdown">
+            <ul class="uk-nav uk-navbar-dropdown-nav">
+              <li><a href="https://www.golinks.io/tdcheck" rel="noopener noreferrer" target="_parent" >Transaction Details Check</a></li>
+              <li><a href="https://www.golinks.io/missingtd" rel="noopener noreferrer" target="_parent" >Missing Transaction Details</a></li>
+            </ul>
+          </div>
+        </li>
+        
+        <li>
           <a href="#">Company-Wide</a>
           <div class="uk-navbar-dropdown">
             <ul class="uk-nav uk-navbar-dropdown-nav">
@@ -22,67 +89,12 @@ $(document).ready(function(){
           </div>
         </li>
         <li>
-          <a href="#">Pipeline & Portfolio</a>
+          <a href="#">Non-Mode Links</a>
           <div class="uk-navbar-dropdown">
             <ul class="uk-nav uk-navbar-dropdown-nav">
-              <li><a href="https://www.golinks.io/expecteddebt" rel="noopener noreferrer" target="_parent" >Expected Debt</a></li>
-              <li><a href="https://www.golinks.io/bbanalytics" rel="noopener noreferrer" target="_parent" >BB Analytics</a></li>
-              
-   
-            </ul>
-          </div>
-        </li>
-        <li>
-          <a href="#">BPO Metrics</a>
-          <div class="uk-navbar-dropdown">
-            <ul class="uk-nav uk-navbar-dropdown-nav">
-              <li><a href="https://www.golinks.io/sse2e" rel="noopener noreferrer" target="_parent" >S&S End-to-End</a></li>
-              <li><a href="https://www.golinks.io/elatracking" rel="noopener noreferrer" target="_parent" >ELA Tracking</a></li>
-              <li><a href="https://www.golinks.io/cepscorecard" rel="noopener noreferrer" target="_parent" >S&S CEP Scorecard</a></li>
-              <li><a href="https://www.golinks.io/resalescorecard" rel="noopener noreferrer" target="_parent" >Resale Scorecard</a></li>
-              <li><a href="https://www.golinks.io/fsscorecard" rel="noopener noreferrer" target="_parent" >Fulfillment Scorecard</a></li>
-              <li><a href="https://www.golinks.io/acqcti" rel="noopener noreferrer" target="_parent" >Acquisition Customer Trust Indicators</a></li>
-               <li><a href="https://www.golinks.io/resalecti" rel="noopener noreferrer" target="_parent" >Resale Customer Trust Indicators</a></li>
-               <li><a href="https://www.golinks.io/ssmortgage" rel="noopener noreferrer" target="_parent" >S&S Mortgage Tracking</a></li>
-            </ul>
-          </div>
-        </li>
-         <li>
-          <a href="#">Deal Team Dashboards</a>
-          <div class="uk-navbar-dropdown">
-                    <ul class="uk-nav uk-navbar-dropdown-nav">
-                        <li class="uk-nav-header">Buyer Product</li>
-                            <li><a href="https://www.golinks.io/buyerokr" rel="noopener noreferrer" target="_parent" >Buyer OKRs</a></li>
-                            <li><a href="https://www.golinks.io/bwod-filterable-funnel" rel="noopener noreferrer" target="_parent" >BwOD Funnel (Filtered)</a></li>
-                            <li><a href="https://www.golinks.io/bwod-funnel" rel="noopener noreferrer" target="_parent" >BwOD User Funnel</a></li>
-                            <li><a href="https://www.golinks.io/buyerwaterfall" rel="noopener noreferrer" target="_parent" >Buyer Waterfall Funnel Segmentation</a></li>
-                            <li><a href="https://www.golinks.io/winning-offers-epod" rel="noopener noreferrer" target="_parent" >Winning Offers EPOD</a></li>              
-                            <li><a href="http://www.golinks.io/buyer-offer-funnel" rel="noopener noreferrer" target="_parent" >Offer Funnel by Source</a></li>
-                        <li class="uk-nav-divider"></li>
-                        <li class="uk-nav-header">Buyer Ops</li>
-                            <li><a href="https://www.golinks.io/agentopsbuyerperformance" rel="noopener noreferrer" target="_parent" >Agent Ops Buyer Performance</a></li>
-                            <li><a href="https://www.golinks.io/cepqbpilot" rel="noopener noreferrer" target="_parent" >CEP-as-QB Pilot</a></li>
-                            <li><a href="https://www.golinks.io/touragents" rel="noopener noreferrer" target="_parent" >Tour Agents Overview</a></li>
-                        <li class="uk-nav-divider"></li>
-                        <li class="uk-nav-header">Opendoor Complete</li>   
-                            <li><a href="https://www.golinks.io/buysell2022okr" rel="noopener noreferrer" target="_parent" >Buy & Sell 2022 OKRs</a></li>
-                    </ul>
-                </div>
-            </li>
-             <li>
-          <a href="#">CapM - Placeholder</a>
-          <div class="uk-navbar-dropdown">
-            <ul class="uk-nav uk-navbar-dropdown-nav">
-              <li><a href="https://www.golinks.io/exceptions" rel="noopener noreferrer" target="_parent" >Lender Finance Exceptions</a></li>
-            </ul>
-          </div>
-        </li>
-        <li>
-          <a href="#">CapM - Placeholder</a>
-          <div class="uk-navbar-dropdown">
-            <ul class="uk-nav uk-navbar-dropdown-nav">
-              <li><a href="https://www.golinks.io/growthokrs" rel="noopener noreferrer" target="_parent" >Consumer Growth OKRs</a></li>
-              <li><a href="https://www.golinks.io/onboardingdash" rel="noopener noreferrer" target="_parent" >Onboarding Flow Dashboard</a></li>
+              <li><a href="https://www.golinks.io/exceptions" rel="noopener noreferrer" target="_parent" >Lender Finance Exception Reporting</a></li>
+              <li><a href="https://www.golinks.io/hcportal" rel="noopener noreferrer" target="_parent" >House Canary Portal</a></li>
+              <li><a href="https://www.golinks.io/ccportal" rel="noopener noreferrer" target="_parent" >Clear Capital Portal</a></li>
             </ul>
           </div>
         </li>
