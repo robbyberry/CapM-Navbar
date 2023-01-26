379c802552e729448b2ee78a1f15c406952347a2
robbyberry <87150464+robbyberry@users.noreply.github.com>
2023-01-26 14:38:50 -0500

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -31,10 +31,6 @@ $(document).ready(function(){
             <ul class="uk-nav uk-navbar-dropdown-nav">
                 <li class="uk-nav-header">Ops Favorites</li>
               <li><a href="https://www.golinks.io/opsmasterfile" rel="noopener noreferrer" target="_parent" >Ops Master File</a></li>
-              <li><a href="https://www.golinks.io/termresold" rel="noopener noreferrer" target="_parent" >Term Resold Reports</a></li>
-              <li><a href="https://www.golinks.io/assetschedules" rel="noopener noreferrer" target="_parent" >Facility Asset Schedules</a></li>
-              <li><a href="https://www.golinks.io/notallocated" rel="noopener noreferrer" target="_parent" >Reporting Tokens Not Allocated</a></li>
-              <li><a href="https://www.golinks.io/mezzpendingreleases" rel="noopener noreferrer" target="_parent" >Mezz Pending Releases</a></li>
               <li class="uk-nav-divider"></li>
                    <li class="uk-nav-header">Reporting</li>
               <li><a href="https://www.golinks.io/termresold" rel="noopener noreferrer" target="_parent" >Term Resold Reports</a></li>
